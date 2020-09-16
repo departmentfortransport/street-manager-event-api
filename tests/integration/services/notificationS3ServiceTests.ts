a97970ec7fcf3c12ec703b6536370df12beb08f1
Caoimhe Boyle <c.boyle@kainos.com>
2020-09-16 15:33:57 +0100

    SM-5579: Add linting rules

--- tests/integration/services/notificationS3ServiceTests.ts
+++ tests/integration/services/notificationS3ServiceTests.ts
@@ -5,7 +5,7 @@ import { getFileFromS3, deleteFileFromS3 } from '../helpers/s3Helper'
 describe('NotificationS3Service Tests', () => {
   let key: string
 
-  before(async () => {
+  before(() => {
     key = 'notifications-integration-test/permit_QRS1590571966998-01_work_start_event_2020-06-02_09-48-00-480.json'
   })
 
